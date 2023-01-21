8e6bd14321f9cd6b100d80e21afce7df3d8baa20
Ludwig Brida <ludwigbrida@gmail.com>
2023-01-21 16:25:30 +0100

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include "models/Vector.hpp"
 
 int main() {
+    Vector vector;
+    vector.x = 0;
     std::cout << "Hello, World!" << std::endl;
 }
---
+++ src/models/Vector.hpp
@@ -0,0 +1,8 @@
+#ifndef GAME_VECTOR_HPP
+#define GAME_VECTOR_HPP
+
+struct Vector {
+    float x, y, z;
+};
+
+#endif
