7c1ca29bd44e23e2949229feab496f119933a798
Ludwig Brida <ludwigbrida@gmail.com>
2023-01-22 15:40:51 +0100

    ...

--- src/models/Matrix/Matrix.cpp
+++ src/models/Matrix/Matrix.cpp
@@ -1,6 +1,17 @@
 #include "Matrix.hpp"
+#include <cmath>
 
 namespace ng {
+
+template <typename T>
+Matrix<T>::Matrix() = default;
+
+template <typename T>
+Matrix<T>::Matrix(T m00, T m01, T m02, T m03, T m10, T m11, T m12, T m13, T m20,
+                  T m21, T m22, T m23, T m30, T m31, T m32, T m33)
+    : m00(m00), m01(m01), m02(m02), m03(m03), m10(m10), m11(m11), m12(m12),
+      m13(m13), m20(m20), m21(m21), m22(m22), m23(m23), m30(m30), m31(m31),
+      m32(m32), m33(m33) {}
 
 template <typename T>
 Matrix<T> Matrix<T>::fromTranslation(const Vector<T>& vector) {
@@ -18,6 +29,27 @@ Matrix<T> Matrix<T>::fromScale(const Vector<T>& vector) {
   matrix.m11 = vector.y;
   matrix.m22 = vector.z;
   return matrix;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::fromPerspective(const T& fieldOfView, const T& aspectRatio,
+                                     const T& near, const T& far) {
+  const T fov = 1 / std::tan(fieldOfView / 2);
+  const T inv = 1 / (near - far);
+
+  auto matrix = Matrix<T>();
+  matrix.m00 = fov / aspectRatio;
+  matrix.m11 = fov;
+  matrix.m22 = (near + far) * inv;
+  matrix.m23 = -1;
+  matrix.m32 = near * far * inv * 2;
+  return matrix;
+}
+
+template <typename T>
+Matrix<T> operator*(const Matrix<T>& matrix1, const Matrix<T>& matrix2) {
+  // todo
+  return {};
 }
 
 template class Matrix<float>;
