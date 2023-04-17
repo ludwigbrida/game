6b6126a235dcf343d8747e7451d6b1db058d6336
Ludwig Brida <ludwigbrida@gmail.com>
2023-04-17 12:57:23 +0200

    ...

---
+++ src/components/TransformComponent.hpp
@@ -0,0 +1,13 @@
+#ifndef GAME_TRANSFORM_COMPONENT_HPP
+#define GAME_TRANSFORM_COMPONENT_HPP
+
+#include "../maths/Quaternion/Quaternion.hpp"
+#include "../maths/Vector/Vector.hpp"
+
+struct TransformComponent {
+  Vector3f position;
+  Quaternion3f rotation;
+  Vector3f scale;
+};
+
+#endif
