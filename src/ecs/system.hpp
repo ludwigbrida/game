40fdf0807eb070b2d93e022108f0273c6ad5e4b7
Ludwig Brida <ludwigbrida@gmail.com>
2023-05-09 00:50:27 +0200

    ...

--- src/ecs/system.hpp
+++ src/ecs/system.hpp
@@ -1,11 +1,9 @@
 #ifndef GAME_SYSTEM_HPP
 #define GAME_SYSTEM_HPP
 
-class Registry;
-
 class System {
 public:
-	virtual void run(Registry& registry, float deltaTime) = 0;
+	virtual void run(class Registry& registry, float deltaTime) = 0;
 	virtual ~System() = default;
 };
 
