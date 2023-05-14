df8757081d40501028cee1ed5756a8420de54f7c
Ludwig Brida <ludwigbrida@gmail.com>
2023-05-15 00:32:29 +0200

    ...

--- src/types/composable.hpp
+++ src/types/composable.hpp
@@ -8,4 +8,7 @@
 template <typename T>
 concept Composable = std::is_base_of<Component, T>::value;
 
+template <typename T>
+concept Composable2 = std::is_trivial<T>::value && !std::is_scalar<T>::value;
+
 #endif
