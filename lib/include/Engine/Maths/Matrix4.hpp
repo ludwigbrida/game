a663c91f4d3eddf6b97ff2c85d5756d4eed80ef1
Ludwig Brida <ludwigbrida@gmail.com>
2023-05-27 00:09:41 +0200

    ...

--- lib/include/Engine/Maths/Matrix4.hpp
+++ lib/include/Engine/Maths/Matrix4.hpp
@@ -28,6 +28,8 @@ public:
 
 	Matrix4(T m00, T m01, T m02, T m03, T m10, T m11, T m12, T m13, T m20, T m21,
 					T m22, T m23, T m30, T m31, T m32, T m33);
+
+	Matrix4<T> operator*(const Matrix4<T>& other) const;
 };
 
 using Matrix4f = Matrix4<Float>;
--- lib/src/Maths/Matrix4.cpp
+++ lib/src/Maths/Matrix4.cpp
@@ -9,6 +9,28 @@ Matrix4<T>::Matrix4(T m00, T m01, T m02, T m03, T m10, T m11, T m12, T m13,
 			m13{m13}, m20{m20}, m21{m21}, m22{m22}, m23{m23}, m30{m30}, m31{m31},
 			m32{m32}, m33{m33} {}
 
+template <IsArithmetic T>
+Matrix4<T> Matrix4<T>::operator*(const Matrix4<T>& other) const {
+	return {
+		m00 * other.m00 + m01 * other.m10 + m02 * other.m20 + m03 * other.m30,
+		m00 * other.m01 + m01 * other.m11 + m02 * other.m21 + m03 * other.m31,
+		m00 * other.m02 + m01 * other.m12 + m02 * other.m22 + m03 * other.m32,
+		m00 * other.m03 + m01 * other.m13 + m02 * other.m23 + m03 * other.m33,
+		m10 * other.m00 + m11 * other.m10 + m12 * other.m20 + m13 * other.m30,
+		m10 * other.m01 + m11 * other.m11 + m12 * other.m21 + m13 * other.m31,
+		m10 * other.m02 + m11 * other.m12 + m12 * other.m22 + m13 * other.m32,
+		m10 * other.m03 + m11 * other.m13 + m12 * other.m23 + m13 * other.m33,
+		m20 * other.m00 + m21 * other.m10 + m22 * other.m20 + m23 * other.m30,
+		m20 * other.m01 + m21 * other.m11 + m22 * other.m21 + m23 * other.m31,
+		m20 * other.m02 + m21 * other.m12 + m22 * other.m22 + m23 * other.m32,
+		m20 * other.m03 + m21 * other.m13 + m22 * other.m23 + m23 * other.m33,
+		m30 * other.m00 + m31 * other.m10 + m32 * other.m20 + m33 * other.m30,
+		m30 * other.m01 + m31 * other.m11 + m32 * other.m21 + m33 * other.m31,
+		m30 * other.m02 + m31 * other.m12 + m32 * other.m22 + m33 * other.m32,
+		m30 * other.m03 + m31 * other.m13 + m32 * other.m23 + m33 * other.m33,
+	};
+}
+
 template class Matrix4<Float>;
 
 }
