6579deae8429cc2ff44ff08b3d74bc9baa420a37
Ludwig Brida <ludwigbrida@gmail.com>
2023-05-29 00:07:32 +0200

    ...

--- lib/src/Systems/Renderer.cpp
+++ lib/src/Systems/Renderer.cpp
@@ -1,18 +1,26 @@
 #include "Renderer.hpp"
 #include "Engine/Core/Registry.hpp"
 #include <Engine/Components/Transform.hpp>
+#include <GL/glew.h>
 
 namespace ng {
 
-void Renderer::update(Registry& registry, State& state, float deltaTime) {
+void Renderer::update(Registry& registry, State& state, Float deltaTime) {
 	auto entities = registry.view<Transform>();
+
+	clear(Color::Black);
 
 	for (auto entity : entities) {
 		auto& transform = registry.get<Transform>(entity);
 	}
 }
 
-void Renderer::clear(const Color& color) const {}
+void Renderer::clear(const Color& color) const {
+	glClearColor(
+		static_cast<Float>(color.r) / 255, static_cast<Float>(color.g) / 255,
+		static_cast<Float>(color.b) / 255, static_cast<Float>(color.a) / 255);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
 
 void Renderer::draw(const Mesh& mesh, const Matrix4f& modelMatrix) const {}
 
