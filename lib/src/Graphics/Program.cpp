75c4109ae65699d0020427737d7bb6bb88e6b0af
Ludwig Brida <ludwigbrida@gmail.com>
2023-05-29 00:43:19 +0200

    ...

--- lib/src/Graphics/Program.cpp
+++ lib/src/Graphics/Program.cpp
@@ -1,3 +1,32 @@
 #include "Program.hpp"
+#include <iostream>
 
-namespace ng {}
+namespace ng {
+
+Program::Program(const char* vertexSource, const char* fragmentSource)
+		: vertexShader{GL_VERTEX_SHADER, vertexSource},
+			fragmentShader{GL_FRAGMENT_SHADER, fragmentSource} {
+	location = create(vertexShader.location, fragmentShader.location);
+}
+
+UInt32 Program::create(UInt32 vertexLocation, UInt32 fragmentLocation) {
+	auto program = glCreateProgram();
+	glAttachShader(program, vertexLocation);
+	glAttachShader(program, fragmentLocation);
+	glLinkProgram(program);
+
+	Int32 success;
+	char infoLog[512];
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+	if (!success) {
+		glGetProgramInfoLog(program, 512, nullptr, infoLog);
+		std::cerr << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexLocation);
+	glDeleteShader(fragmentLocation);
+	return program;
+}
+
+}
--- lib/src/Graphics/Program.hpp
+++ lib/src/Graphics/Program.hpp
@@ -6,9 +6,16 @@
 namespace ng {
 
 class Program {
+public:
+	Program(const char* vertexSource, const char* fragmentSource);
+
+	UInt32 location;
+
 private:
 	Shader vertexShader;
 	Shader fragmentShader;
+
+	static UInt32 create(UInt32 vertexLocation, UInt32 fragmentLocation);
 };
 
 }
