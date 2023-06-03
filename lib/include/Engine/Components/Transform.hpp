7e3ee2ef62d035b2fee09a1262744cbee0528dfc
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-04 01:14:57 +0200

    ...

--- lib/include/Engine/Components/Transform.hpp
+++ lib/include/Engine/Components/Transform.hpp
@@ -2,13 +2,14 @@
 #define ENGINE_TRANSFORM_HPP
 
 #include <Engine/Core/Component.hpp>
+#include <Engine/Maths/Quaternion.hpp>
 #include <Engine/Maths/Vector3.hpp>
 
 namespace ng {
 
 struct Transform : Component {
 	Vector3f position;
-	// Quaternion rotation;
+	Quaternion<Float> rotation;
 	Vector3f scale{1, 1, 1};
 };
 
