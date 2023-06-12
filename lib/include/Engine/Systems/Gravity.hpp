c3be1b6ed648df76e1f4d8a34e904fa0257beca9
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-13 01:03:22 +0200

    ...

--- lib/include/Engine/Systems/Gravity.hpp
+++ lib/include/Engine/Systems/Gravity.hpp
@@ -8,9 +8,11 @@ namespace ng {
 // TODO: Why doesn't inheritance work properly without "public"
 class Gravity: public System {
 public:
+	Gravity(Float acceleration): acceleration{acceleration} {}
+
 	void update(Registry& registry, State& state, const Clock& clock) override;
 
-	Float acceleration{};
+	Float acceleration;
 };
 
 }
