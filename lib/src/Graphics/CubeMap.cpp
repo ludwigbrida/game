48556ed360fecc60a09cf91136de0140b13b8900
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-17 19:48:18 +0200

    ...

--- lib/src/Graphics/CubeMap.cpp
+++ lib/src/Graphics/CubeMap.cpp
@@ -1,11 +1,37 @@
 #include "CubeMap.hpp"
 #include <GL/glew.h>
+#include <stb_image.h>
 
 namespace ng {
 
 CubeMap::CubeMap() {
 	glGenTextures(1, &id);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, id);
+
+	int width, height, nrChannels;
+	unsigned char* data;
+
+	for (unsigned int i = 0; i < textures.size(); i++) {
+		data = stbi_load(textures[i].c_str(), &width, &height, &nrChannels, 0);
+
+		glTexImage2D(
+			GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+			0,
+			GL_RGB,
+			width,
+			height,
+			0,
+			GL_RGB,
+			GL_UNSIGNED_BYTE,
+			data
+		);
+
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	}
 }
 
 }
