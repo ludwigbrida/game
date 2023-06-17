a39610ba5e333178edf98a2f10911e89dff73188
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-17 21:51:05 +0200

    ...

---
+++ lib/src/Graphics/Texture.cpp
@@ -0,0 +1,47 @@
+#include "Texture.hpp"
+#include <GL/glew.h>
+#include <iostream>
+#include <stb_image.h>
+
+namespace ng {
+
+Texture::Texture(const std::string& source) {
+	glGenTextures(1, &id);
+	glBindTexture(GL_TEXTURE_2D, id);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(
+		GL_TEXTURE_2D,
+		GL_TEXTURE_MIN_FILTER,
+		GL_LINEAR_MIPMAP_LINEAR
+	);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	unsigned char* data;
+
+	data = stbi_load(source.c_str(), &width, &height, &nrChannels, 0);
+
+	if (data) {
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RGB,
+			width,
+			height,
+			0,
+			GL_RGB,
+			GL_UNSIGNED_BYTE,
+			data
+		);
+
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		std::cout << "Could not load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+}
+
+}
--- lib/src/Graphics/Texture.hpp
+++ lib/src/Graphics/Texture.hpp
@@ -1,9 +1,18 @@
 #ifndef GAME_TEXTURE_HPP
 #define GAME_TEXTURE_HPP
 
+#include <Engine/Core/Arithmetic.hpp>
+#include <string>
+
 namespace ng {
 
-class Texture {};
+class Texture {
+public:
+	Texture(const std::string& source);
+
+private:
+	UInt32 id{};
+};
 
 }
 
