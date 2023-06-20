c6c14bdd4b8fdff1b3d762f162951d3f48ae8f50
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-20 17:31:34 +0200

    ...

--- lib/src/Graphics/Texture.cpp
+++ lib/src/Graphics/Texture.cpp
@@ -22,20 +22,23 @@ Texture::Texture(const std::string& source): textureId{0} {
 	glGenTextures(1, &textureId);
 	glBindTexture(GL_TEXTURE_2D, textureId);
 
-	glTexParameteri(textureId, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(textureId, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	glTexSubImage2D(
-		textureId,
+	glTexImage2D(
+		GL_TEXTURE_2D,
 		0,
-		0,
-		0,
+		GL_RGB,
 		width,
 		height,
+		0,
 		GL_RGB,
 		GL_UNSIGNED_BYTE,
 		data
 	);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	stbi_image_free(data);
 }
