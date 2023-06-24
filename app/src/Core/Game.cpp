3cc2b7070424b00bcdeea545a80213da4d25968e
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-24 14:42:45 +0200

    ...

--- app/src/Core/Game.cpp
+++ app/src/Core/Game.cpp
@@ -4,8 +4,16 @@
 
 namespace Engine {
 
-struct Transform2 {
+struct Component2 {};
+
+struct Transform2: Component2 {
 	Vector3<Float> position;
+	Quaternion<Float> rotation;
+	Vector3<Float> scale;
+};
+
+struct Position: Vector3<Float>, Component2 {
+	using Vector3<Float>::Vector3;
 };
 
 }
@@ -13,9 +21,15 @@ struct Transform2 {
 Game::Game() {
 	registry.activate<Movement>();
 
-	Engine::Transform2 transform = {{0, 0, 0}};
+	Engine::Transform2 transform = {
+		.position = {0, 0, 0},
+		.rotation = {0, 0, 0, 1},
+		.scale = {1, 1, 1}};
 
-	registry.add<Engine::Transform>(0);
+	std::unique_ptr<Engine::Component2> com =
+		std::make_unique<Engine::Position>(0, 0, 0);
+
+	registry.add<Engine::Transform>(0, transform);
 
 	registry.remove<Engine::Transform>(0);
 
