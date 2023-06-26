e8960f57e788098abad155f5a8dac9bbc2e809b1
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-26 23:49:03 +0200

    ...

--- app/src/Core/Game.cpp
+++ app/src/Core/Game.cpp
@@ -12,11 +12,41 @@ Game::Game() {
 	constexpr auto player = 0;
 	registry.add<Engine::Transform>(player, Engine::Transform());
 	registry.add<Engine::Perspective>(player, Engine::Perspective());
+	registry.add<Engine::Collider>(
+		player,
+		{.vertices{
+			{-.5, .5, -.5},
+			{.5, .5, -.5},
+			{-.5, .5, .5},
+			{.5, .5, .5},
+			{-.5, -.5, -.5},
+			{.5, -.5, -.5},
+			{-.5, -.5, .5},
+			{.5, -.5, .5},
+		}}
+	);
 
 	constexpr auto triangle = 1;
 	registry.add<Engine::Transform>(triangle, {.position = {-1, 0, -2}});
 	registry.add<Engine::Mesh>(triangle, Engine::Mesh::createTriangle(1));
 	registry.add<Engine::Physics>(triangle, Engine::Physics());
+
+	constexpr auto cube = 2;
+	registry.add<Engine::Transform>(cube, {.position{1, 0, -2}});
+	registry.add<Engine::Mesh>(cube, Engine::Mesh::createCube(1));
+	registry.add<Engine::Collider>(
+		cube,
+		{.vertices{
+			{-.5, .5, -.5},
+			{.5, .5, -.5},
+			{-.5, .5, .5},
+			{.5, .5, .5},
+			{-.5, -.5, -.5},
+			{.5, -.5, -.5},
+			{-.5, -.5, .5},
+			{.5, -.5, .5},
+		}}
+	);
 
 	state.activeCamera = player;
 
