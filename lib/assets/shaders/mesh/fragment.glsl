10ba0b383e6b21931612f49f00051d98ba6ee998
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-29 00:48:05 +0200

    ...

--- lib/assets/shaders/mesh/fragment.glsl
+++ lib/assets/shaders/mesh/fragment.glsl
@@ -9,5 +9,5 @@ out vec4 color;
 
 void main() {
 	// color = vec4(fragmentColor, 1);
-	color = texture(diffuseTexture, fragmentTexCoord);
+	color = texture(diffuseTexture, fragmentTexCoord) * vec4(fragmentColor, 1);
 }
