13d0dcdf223b6ee08a26a436a1f2b0be3bd80d4e
Ludwig Brida <ludwigbrida@gmail.com>
2023-06-30 00:26:51 +0200

    ...

--- lib/src/Input/Mouse.cpp
+++ lib/src/Input/Mouse.cpp
@@ -2,10 +2,22 @@
 
 namespace Engine::Mouse {
 
-void callback(GLFWwindow* window, double positionX, double positionY) {}
+void cursorPositionCallback(
+	GLFWwindow* window,
+	double positionX,
+	double positionY
+) {}
+
+void cursorEnterCallback(GLFWwindow* window, int entered) {}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {}
+}
 
 void setup(GLFWwindow* window) {
-	glfwSetCursorPosCallback(window, callback);
+	glfwSetCursorPosCallback(window, cursorPositionCallback);
+	glfwSetCursorEnterCallback(window, cursorEnterCallback);
+	glfwSetMouseButtonCallback(window, mouseButtonCallback);
 	// glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 }
 
