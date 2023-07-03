d778e9e1fecf915e4b8e1557cf3876625dbe133c
Ludwig Brida <ludwigbrida@gmail.com>
2023-07-04 01:43:21 +0200

    ...

--- lib/assets/shaders/skybox/vertex.glsl
+++ lib/assets/shaders/skybox/vertex.glsl
@@ -8,8 +8,7 @@ layout (location = 0) in vec3 vertexTexCoord;
 out vec3 fragmentTexCoord;
 
 void main() {
-	// fragmentTexCoord = vertexTexCoord;
-	fragmentTexCoord = vec3(vertexTexCoord.x, vertexTexCoord.y, -vertexTexCoord.z);
+	fragmentTexCoord = vertexTexCoord;
 	vec4 position = projectionMatrix * viewMatrix * vec4(vertexTexCoord, 1);
-	gl_Position = vec4(position.x, position.y, position.w, position.w);
+	gl_Position = position.xyww;
 }
